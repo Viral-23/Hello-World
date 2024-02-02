c5e8376a1b5526fc2401a959a616fe2ea7b2d58b
Viral-23 <73192398+Viral-23@users.noreply.github.com>
2024-02-02 11:05:29 -0500

    Initial Commit

---
+++ Hello World/Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldApp.swift
+//  Hello World
+//
+//  Created by Viral Patel on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
